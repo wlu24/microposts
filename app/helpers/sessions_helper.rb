ae27872f5dd14c0022bcb5bf80f28306cbc301a2
Wayne Lu <waynelu24lon@gmail.com>
2017-12-31 11:40:33 +0000

    Add account activation
    "11.4 Email in production" was left undone because it requires
    entering credit card information to the Heroku account. Revisit
    this part if the functionality is needed.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -48,7 +48,7 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
 
   # Redirects to stored location (or to the default).
   def redirect_back_or(default)
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
 
   resources :users
+  resources :account_activations, only: [:edit]
 end
