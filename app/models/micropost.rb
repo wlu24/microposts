7116d8fb96ed88605901c97ee8fd7ee3e490450d
Wayne Lu <waynelu24lon@gmail.com>
2018-01-01 23:15:48 +0000

    Image validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,4 +10,17 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
+  validate  :picture_size # use validate (no 's') to call custom validation
+
+
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+
 end
